670dca0d420efeb28bffc6345dcec16d2fff748e
Patricia Bresku <patriciabrsk@192.168.1.62>
2022-05-27 18:55:29 +0200

    add front office

---
+++ resources/js/views/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>WIP</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    components: {},
+
+}
+</script>
+
+<style scoped>
+
+</style>
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/admin.js') }}"></script>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600&display=swap" rel="stylesheet">
